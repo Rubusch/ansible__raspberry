67d896fafeca6858b23ad5e3b43c37103d939c90
Lothar Rubusch <l.rubusch@gmail.com>
2022-10-08 01:25:08 +0200

    scrips: quick&dirty provisioning SD script
    Signed-off-by: Lothar Rubusch <l.rubusch@gmail.com>

---
+++ setup.sh
@@ -0,0 +1,31 @@
+#!/bin/sh -e
+##
+## provide e.g. /dev/sdi, when there is an /dev/sdi1 and /dev/sdi2
+##
+## - provide rootfs secrets under "secrets"
+## - try to make sure you have sudo permissions
+
+DEV="${1}"
+
+if [ -z "${DEV}" ]; then
+	echo "usage: ${0} <dev of SD card>"
+	exit 1
+fi
+
+sudo dd if=download/2022-09-22-raspios-bullseye-arm64-lite.img  of=/dev/sdi bs=4M conv=fdatasync status=progress
+
+udisksctl mount -b "${DEV}1"
+sudo cp -arf ./rootfs/boot/* /media/${USER}/boot/
+udisksctl unmount -b "${DEV}1"
+
+udisksctl mount -b "${DEV}2"
+sudo cp -arf ./rootfs/etc /media/${USER}/rootfs/
+sudo cp -arf ./rootfs/root /media/${USER}/rootfs/
+cp -arf ./rootfs/home/pi /media/${USER}/rootfs/home/
+
+sudo cp -arf ./secret/etc /media/${USER}/rootfs/
+cp -arf ./secret/home/pi /media/${USER}/rootfs/home/
+udisksctl unmount -b "${DEV}2"
+
+echo "READY."
+
