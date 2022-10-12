0f370e370ad13062488b2e6ef68dd7cef527407d
Lothar Rubusch <l.rubusch@gmail.com>
2022-10-13 01:33:22 +0200

    setup.sh: update
    Signed-off-by: Lothar Rubusch <l.rubusch@gmail.com>

--- setup.sh
+++ setup.sh
@@ -16,17 +16,25 @@ fi
 sudo dd if="${IMG}" of="${DEV}" bs=4M conv=fdatasync status=progress
 sleep 5
 
+
+## boot
 udisksctl mount -b "${DEV}1"
-sudo cp -arf ./rootfs/boot/* /media/${USER}/boot/
+sudo cp -arfv ./rootfs/boot/* /media/${USER}/boot/
 udisksctl unmount -b "${DEV}1"
 
+
+## rootfs
 udisksctl mount -b "${DEV}2"
-sudo cp -arf ./rootfs/etc /media/${USER}/rootfs/
-sudo cp -arf ./rootfs/root /media/${USER}/rootfs/
+sudo cp -arfv ./rootfs/etc /media/${USER}/rootfs/
+sudo cp -arfv ./rootfs/root /media/${USER}/rootfs/
 cp -arf ./rootfs/home/pi /media/${USER}/rootfs/home/
 
-sudo cp -arf ./secret/etc /media/${USER}/rootfs/
-cp -arf ./secret/home/pi /media/${USER}/rootfs/home/
+sudo cp -arfv ./secret/etc /media/${USER}/rootfs/
+cp -arfv ./secret/home/pi /media/${USER}/rootfs/home/
+
+## rootfs - remove dhcpcd (we use dnsmasq)
+sudo rm -fv /media/${USER}/rootfs/etc/systemd/system/multi-user.target.wants/dhcpcd.service
+
 udisksctl unmount -b "${DEV}2"
 
 echo "READY."
