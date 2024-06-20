cc4c50b150725eac518cb98533bd2908b7f704b0
Lothar Rubusch <l.rubusch@gmail.com>
2024-06-20 19:06:53 +0100

    ansible: add mod-xilinxsrv
    provide working xilinx hw_server setup for 2023.1
    
    Signed-off-by: Lothar Rubusch <l.rubusch@gmail.com>

--- ansible/mod-xilinxsrv/files/hw_server_23.1.sh
+++ ansible/mod-xilinxsrv/files/hw_server_23.1.sh
@@ -2,16 +2,19 @@
 #
 # start: start qemu hw_server
 # stop: stop qemu hw_server
+SERVERIP="10.1.10.65"
+XILINX_VERSION="2023.1"
+
 ARG=$1
 case "$ARG" in
 "start")
 	test -f /run/hw_server.pid && exit 1
-	test -z $( mount | grep "/x86/proc" ) && mount -t proc /proc /x86/proc
-	test -z $( mount | grep "/x86/sys" ) && mount -t sysfs /sys /x86/sys
-	test -z $( mount | grep "/x86/dev" ) && mount -o bind /dev /x86/dev
-	test -z $( mount | grep "/x86/dev/pts" ) && mount -t devpts /dev/pts /x86/dev/pts
+	test -z $( mount | grep "/x86/proc" ) && mount -t proc /proc /x86/proc || true
+	test -z $( mount | grep "/x86/sys" ) && mount -t sysfs /sys /x86/sys || true
+	test -z $( mount | grep "/x86/dev" ) && mount -o bind /dev /x86/dev || true
+	test -z $( mount | grep "/x86/dev/pts" ) && mount -t devpts /dev/pts /x86/dev/pts || true
 	echo $$ > /run/hw_server.pid
-	chroot /x86 qemu-x86_64-static /usr/bin/bash -c "source /xilinx/Vivado_Lab/2023.1/settings64.sh ; hw_server -s tcp:10.1.10.65:3121"
+	chroot /x86 qemu-x86_64-static /usr/bin/bash -c "source /xilinx/Vivado_Lab/${XILINX_VERSION}/settings64.sh ; hw_server -s tcp:${SERVERIP}:3121"
 	## the above shall not return, if it fails remove the pid file
 	rm -f /run/hw_server.pid || true
 	;;
@@ -28,7 +31,7 @@ case "$ARG" in
 	test -z $( mount | grep "/x86/sys" ) && mount -t sysfs /sys /x86/sys
 	test -z $( mount | grep "/x86/dev" ) && mount -o bind /dev /x86/dev
 	test -z $( mount | grep "/x86/dev/pts" ) && mount -t devpts /dev/pts /x86/dev/pts
-	chroot /x86 qemu-x86_64-static /usr/bin/bash -c "source /xilinx/Vivado_Lab/2023.1/settings64.sh ; hw_server -L/var/hw_server.log -lprotocol -s tcp:10.1.10.65:3121"
+	chroot /x86 qemu-x86_64-static /usr/bin/bash -c "source /xilinx/Vivado_Lab/${XILINX_VERSION}/settings64.sh ; hw_server -L/var/hw_server.log -lprotocol -s tcp:${SERVERIP}:3121"
 	;;
 *)
 	echo "FAILED"
